14c43c8329e4459404adbf620c47e8492024913a
Brett Bender <dev@justadev.net>
2018-12-16 16:17:12 -0600

    Page loader,  minimal changes

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/*
+Main.js
+
+*/
+
+$(document).imagesLoaded( function() {
+    $('body').addClass('loaded');
+});
+
+$(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+
+    });
+});
