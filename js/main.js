17c806c673c7504edaf0371657b68b187453b188
Brett Bender <dev@justadev.net>
2019-01-22 11:59:42 -0600

    Add in full content, update a few things.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
-/*
+/* 
 Main.js
 
 */
 
+
+// Check for click events on the navbar burger icon
+$("#nav-burger").click(function() {
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $("#nav-burger").toggleClass("is-active");
+    $("#nav-menu").toggleClass("is-active");
+
+});
+
 $(document).imagesLoaded( function() {
-    $('body').addClass('loaded');
+  	$('body').addClass('loaded');
 });
 
 $(document).ready(function() {
 
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").toggleClass("is-active");
-
+    $('#showModal').click(function() {
+        $('.modal').addClass("is-active");
+    });
+    $(".modal-close").click(function() {
+        $(".modal").removeClass("is-active");
+    });
+    $(".modal-background").click(function() {
+        $(".modal").removeClass("is-active");
     });
 });
